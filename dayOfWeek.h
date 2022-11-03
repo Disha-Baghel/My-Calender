81288af34f665063b4a2f399a827c38c61ef4115
Disha-Baghel <bagheldisha708@gmail.com>
2022-11-03 21:57:47 +0530

    Added new file

---
+++ dayOfWeek.h
@@ -0,0 +1,9 @@
+//Zeller's Rule
+//Formula Used
+//F = k+[(13*m-1)/5+D+[D/4]+[C/4]-2*C]
+//Where 
+//k is the day of the month
+//m is the month number
+//D is the last two digits of the year
+//C is the first two digits of the year
+
