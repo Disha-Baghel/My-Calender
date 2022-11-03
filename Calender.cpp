1954314c26d9c933f8e054cb6ecd2500f9d85c94
Disha-Baghel <bagheldisha708@gmail.com>
2022-11-04 02:41:55 +0530

    Added new file

--- Calender.cpp
+++ Calender.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+//#include "dayOfWeek.h"
 
 using namespace std;
 class Calender{
@@ -15,10 +16,104 @@ class Calender{
     int getMonth(){ return this->month; }
     int getYear(){ return this->year; }
     void setYear(){ this->year = this->year - 1000; }
-}calender;
 
-int checkLeapYear(){
-    if(calender.getYear()%400 == 0 || (calender.getYear()%100 != 0 && calender.getYear()%4 == 0)){
+    const char* Zellercongruence(){
+    if (month == 1) {
+        month = 13;
+        year--;
+    }
+    if (month == 2) {
+        month = 14;
+        year--;
+    }
+    int q = date;
+    int m = month;
+    int k = year % 100;
+    int j = year / 100;
+    int h = q + 13 * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+    h = h % 7;
+    switch (h) {
+        case 0:
+        return "Saturday";
+        case 1:
+        return "Sunday";
+        case 2:
+        return "Monday";
+        case 3:
+        return "Tuesday";
+        case 4:
+        return "Wednesday";
+        case 5:
+        return "Thursday";
+        case 6:
+        return "Friday";
+        }
+        return 0;
+    }
+
+    void printDate(){
+        cout<<"-----------------\n";
+        switch(month){
+            case 1: cout<<"January"; break;
+            case 2: cout<<"February"; break;
+            case 3: cout<<"March"; break;
+            case 4: cout<<"April"; break;
+            case 5: cout<<"May"; break;
+            case 6: cout<<"June"; break;
+            case 7: cout<<"July"; break;
+            case 8: cout<<"August"; break;
+            case 9: cout<<"September"; break;
+            case 10: cout<<"October"; break;
+            case 11: cout<<"November"; break;
+            case 12: cout<<"December"; break;
+        }
+        cout<<", "<<year;
+        cout<<"\n---------------------\n";
+    }
+
+    void printMonth(int x, int y){
+        int nod, day, cnt, d = 1, x1 = x, y1 = y, isNote = 0;
+        if(!(month>=1 && month<=12)){
+            cout<<"Invalid Month";
+            return ;
+        } 
+        if(!(year >=1600)){
+            cout<<"Invalid Month";
+            return ;
+        }
+        printDate();
+        y += 3;
+        cout<<"S    M    T    W    Th    F    S\n";
+        y++;
+        nod = getNumberOfDays(month, year);
+        day = getDayNumber(d, month, year);
+        switch(day){
+            case 0: 
+            x=x;
+            cnt =1;
+            break;
+            case 1: 
+            x = x + 4;
+            cnt = 2;
+            break;
+            case 2: 
+            x = x + 8;
+            cnt = 3;
+            break;
+            case 3:
+            x = x + 12;
+            cnt = 4;
+            break;
+            case 4: 
+            x = x + 16;
+        }
+
+    }
+
+};
+
+int checkLeapYear(int year){
+    if(year%400 == 0 || (year%100 != 0 && year%4 == 0)){
         return 1;
     }
     return 0;
@@ -30,7 +125,7 @@ int getNumberOfDays(int month, int year){
            return 30;
 	    }
         else if (month == 2){
-            if(checkLeapYear()==1)
+            if(checkLeapYear(year)==1)
                 return 29;
             else
                 return 28;
@@ -50,9 +145,9 @@ int getNumberOfDays(int month, int year){
 }
 
 int getDayNumber(int date, int month, int year){
-    int rem = 0, t1, t2, y = calender.getYear();
+    int rem = 0, t1, t2, y = year;
     // calender.getYear() = calender.getYear() - 1000;
-    calender.setYear();
+    year = year - 1600;
     while (year >= 100)
     {
         rem = rem + 5;
@@ -79,23 +174,42 @@ int getDayNumber(int date, int month, int year){
     return rem;
 }
 
+// const char* getName(int day){
+//     switch (day){
+//         case 3: return "Sunday";
+//         case 4: return "Monday";
+//         case 5: return "Tuesday";
+//         case 6: return "Wednesday";
+//         case 7: return "Thursday";
+//         case 1: return "Friday";
+//         case 2: return "Saturday";
+//         default: return "Error";
+//     }
+// }
 
-const char *getDay(){
-    int d = 0;
-    if(calender.getMonth()<1 && calender.getMonth()>12){
-        return ("invalid month");
-    }
-    if(calender.getDate()<1 && calender.getDate()>getNumberOfDays(calender.getMonth(), calender.getYear())){
-        return ("Invalid Date");
-    }
-    if(calender.getYear()>=1000){
-        d = getDayNumber(calender.getDate(), calender.getMonth(), calender.getYear());
-    }
-}
+// const char *getDay(){
+//     int d = 0;
+//     if(calender.getMonth()<1 && calender.getMonth()>12){
+//         return ("invalid month");
+//     }
+//     if(calender.getDate()<1 && calender.getDate()>getNumberOfDays(calender.getMonth(), calender.getYear())){
+//         return ("Invalid Date");
+//     }
+//     if(calender.getYear()>=1000){
+//         d = getDayNumber(calender.getDate(), calender.getMonth(), calender.getYear());
+//         d = d%7;
+//         return (getName(d));
+//     }
+//     else{
+//         return ("Please give year more 1000");
+//     }
+// }
+
 int main(){
     int choice;
     int date, month, year;
-    // Calender calender;
+    char ch = 'a';
+    Calender calender;
     cout<<"Press 1 to find out the day"<<endl;
     cout<<"Press 2 to print all the day of month"<<endl;
     cout<<"Press 3 to add note"<<endl;
@@ -108,8 +222,14 @@ int main(){
         cout<<"Enter date (DD MM YYYY) : ";
         cin>>date>>month>>year;
         calender.setData(date, month, year);
-        cout<<"Day : "<<getDay();
+        cout<<"Day : "<<calender.Zellercongruence();
         break;
+    case 2: 
+        cout<<"Enter month and year (MM YYYY) : ";
+        cin>>month>>year;
+        while(ch != 'q'){
+            calender.printMonth(20, 5);
+        }
 
     default:
         ;
--- dayOfWeek.h
+++ dayOfWeek.h
@@ -7,3 +7,63 @@
 //D is the last two digits of the year
 //C is the first two digits of the year
 
+// #include <cmath>
+// #include <cstring>
+// #include <iostream>
+// using namespace std;
+ 
+const char* Zellercongruence(int day, int month, int year)
+{
+    if (month == 1) {
+        month = 13;
+        year--;
+    }
+    if (month == 2) {
+        month = 14;
+        year--;
+    }
+    int q = day;
+    int m = month;
+    int k = year % 100;
+    int j = year / 100;
+    int h = q + 13 * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+    h = h % 7;
+    switch (h) {
+    case 0:
+        //cout << "Saturday \n";
+        return "Saturday";
+        break;
+    case 1:
+        //cout << "Sunday \n";
+        return "Sunday";
+        break;
+    case 2:
+        //cout << "Monday \n";
+        return "Monday";
+        break;
+    case 3:
+        //cout << "Tuesday \n";
+        return "Tuesday";
+        break;
+    case 4:
+        //cout << "Wednesday \n";
+        return "Wednesday";
+        break;
+    case 5:
+        //cout << "Thursday \n";
+        return "Thursday";
+        break;
+    case 6:
+        //cout << "Friday \n";
+        return "Friday";
+        break;
+    }
+    return 0;
+}
+ 
+// // Driver code
+// int main()
+// {
+//     Zellercongruence(22, 10, 2017); // date (dd/mm/yyyy)
+//     return 0;
+// }
