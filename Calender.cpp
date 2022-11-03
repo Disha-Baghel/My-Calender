a4139ab04b52d276205cbbbcf82adee49697a148
Disha-Baghel <bagheldisha708@gmail.com>
2022-11-03 20:30:16 +0530

    Added new file

--- Calender.cpp
+++ Calender.cpp
@@ -25,26 +25,6 @@ int checkLeapYear(){
 }
 
 int getNumberOfDays(int month, int year){
-    // switch (month)
-    // {
-    // case 1: return 31;
-    // case 2: if(checkLeapYear()==1)
-    //             return 29;
-    //         else
-    //             return 28;
-    // case 3: return 31;
-    // case 4: return 30;
-    // case 5: return 31;
-    // case 6: return 30;
-    // case 7: return 31;
-    // case 8: return 31;
-    // case 9: return 30;
-    // case 10: return 31;
-    // case 11: return 30;
-    // case 12: return 31;      
-    // default:
-    //     return -1;
-    // }
     if (month<=7){
         if(month%2 == 0 && month != 2) {
            return 30;
